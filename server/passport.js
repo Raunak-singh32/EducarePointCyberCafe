3760678f282ba67358700ad371d1186248c40182
Raunak Singh <2005sraunak1@gmail.com>
2026-07-13 14:15:15 +0530

    Fix Google OAuth callback URL

--- server/passport.js
+++ server/passport.js
@@ -5,7 +5,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/api/auth/google/callback'
+        callbackURL: 'https://educarepointcybercafe-1.onrender.com/api/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
         try {
